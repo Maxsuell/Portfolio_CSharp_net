0dafd6deae43a6c73ced547491f96fdc65c5f759
Maxsuell <Maxsuell001@outlook.com>
2020-09-06 01:30:59 -0300

    Primeiro commit - Projeto iniciado

---
+++ BEndCsharp_Teste1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BEndCsharp_Teste1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
